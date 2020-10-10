fe068693d6baf4a9d7d1cfd78994d192dd36dcb3
Robert McCaull <romccaull@gyoza.reed.edu>
2020-10-10 16:56:07 -0700

    latest pzip.c

--- pzip.c
+++ pzip.c
@@ -36,23 +36,63 @@ long* work_orders;
 long threads;
 // set up ^ so that work_orders[i] is the 'start' of the work for thread i
 // thread i reads from byte i to byte work_orders[i+1]
+struct compthread_args {
+  int worker_id; // starts at 0
+  long* job_sizes;
+  int num_jobs;
+  char* target_file_name;
+}
+// expects arg to be a pointer to a valid compthread_arg struct (?)
+//  we need id, the list of job sizes (but that can be public), and the file name (can also be public)
+//  so all we really need, strictly speaking, is the id
+
+// but we should probably pass arguments by struct anyway
+
+//TODO: add to README that we're assuming this runs on an SSD (parallel reads)
+
 void* compressor_thread(void* arg) {
-    long long int retval;
+    struct compthread_args* args = (compthread_arg*) arg;
     FILE *fp;
     const char* mode = "r";
-    char* arg_s = (char *) arg;
-    printf("opening file: %s\n", arg_s);
-    fp = fopen(arg_s, mode);
-    char buff[127];
-    if(fgets(buff, 127, fp)) {
-        printf("content: %s\n", buff);
-        retval = 0;
-    } else {
-        retval = 1;
+    printf("Worker %d is opening file: %s\n", args->worker_id, arg_s);
+    fp = fopen(args->target_file_name, mode); //TODO: check if this worked!
+
+    fseek(fp, args->job_sizes[args->worker_id], SEEK_SET); //TODO: check if this worked!
+
+    char curr_char = fgetc(fp);
+    long curr_char_count = 1;
+    if (args->worker_id != 0) { // do the scan-forward thing
+      char new_char = fgetc(fp);
+      while (new_char == curr_char) {
+        curr_char_count = curr_char_count + 1;
+      }
+      curr_char = new_char;
+      curr_char_count = 1;
     }
-    fclose(fp);
-    return (void *) retval;
+
+  while(true) {
+    if ((args->worker_id != args->num_jobs-1) && (ftell(fp) > args->job_sizes[args->worker_id])
+        || (curr_char == EOF)) {
+      break;
+    }
+    for (count = 1; (new_char = fgetc(fp)) == curr_char; count++);
+    //add to the to-write struct
+  }
+  fclose(fp);
+  return (void *) retval;
 }
+
+// printing this should be
+struct compressor_return {
+  long long length;
+  uint32* counts;
+  char* chars;
+}
+
+//Compressor-thread returns (via join) a void-casted pointer to (? thing you're supposed to write)
+
+// we get an input AND output thread?!?! Then we should definitely use an output thread. . .
+// think about how to make that work . . . the issue is still that thread 1 has to complete
 
 int main(int argc, char** argv) {
     // int NUM_CORES = get_nprocs_conf();
@@ -67,12 +107,16 @@ int main(int argc, char** argv) {
     work_orders = (long *)calloc(threads, sizeof(long));
     work_orders[0] = 0;
     int* file_sizes = get_file_sizes(argv);
-
-    // TODO: come up with algorithm for assigning n threads to k files in some intelligent way
-    // int jobsize = work / threads;
-    // for (int i = 1; i < threads; i++) {
-    //   work_orders[i] = work_orders[i-1] + jobsize;
-    // }
+    int work = 0;
+    for (int i = 0; i < sizeof(file_sizes) / sizeof(int); i++) {
+        work += file_sizes[i];
+    }
+    // work_orders[i] is the start byte for the i'th thread
+    //
+    int jobsize = work / threads;
+    for (int i = 1; i < threads; i++) {
+      work_orders[i] = work_orders[i-1] + jobsize;
+    }
 
     // char fname[] = {'h','e','l','l','o','.','t','x','t','\0'};
     // void *argptr = (void* )fname;
