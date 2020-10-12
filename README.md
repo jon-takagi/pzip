0bd7580431d1f09bfd7e971f1d29d98a5d87243d
Jon Takagi <jontakagi@gyoza.reed.edu>
2020-10-12 02:06:30 -0700

    we done

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 ## Parallel ZIP
 #### Jon Takagi and Robert McCaull
 This solution is based on Eitan's rzip, also included in this code.
+
+Our solution follows Eitan's solution (rather than the book's) behavior when multiple files are passed.
+When Eitan's rzip is passed multiple files, it processes each file individually, then appends it to the result of the previous file. The book's solution expects that the input files are concatenated together then processed.
+As an example, consider the file containing "AABBB" and the file "BBCC". While the book would expect 2A5B2C (with binary numbers), our code would output "2A3B2B2C".
+
+Furthermore, our solution assumes hardware support for parallel reads from storage. While this assumption holds for solid state disks, it is not true of hard disk drives.
+
+#### `main`
+Main begins by creating `all_thread_args`. Each element of the array is a struct containing the arguments for a worker thread - its worker ID, the file it will process, its offset inside that file, whether it runs to the end of file or to the start of the next thread, and a completion indicator.
+
+During the first phase of execution, a semaphore is used to ensure that the maximum allowed number of threads are running concurrently. If main sees that there is room for another thread, it starts one.
+
+Once the final set of threads has been created, main waits for them to finish using `pthread_join`. As they return, main writes their outputs to stdout.
+
+Finally, all our memory is free'd. Overall, this program uses a lot of heap memory, but it's all released at the end.
+
+#### `compressor_thread`
+The compressor thread opens its specified file, and scrolls to its specified starting location. It then finds the first new sequence of characters beginning after its specified start location. It then works from there up to the start location of the next specified thread, completing its sequence.
--- pzip.c
+++ pzip.c
@@ -15,34 +15,79 @@
 //     output:  file size in bytes as an int
 long file_size(char *file_name){
     FILE *fp;
-    // printf("opening file %s\n", file_name);
     fp = fopen(file_name, "r");
-    // printf("seeking to end of file %s\n", file_name);
     fseek(fp, 0, SEEK_END);
     long size = ftell(fp);
     fclose(fp);
-    // printf("closing file %s\n", file_name);
     return size;
 }
 
 
-// function: compressor_thread
 
-/* function - compressor_thread
+/*  function: compressor_thread
+**    input (via void-casted pointer to a compthread_args struct):
+**      worker_id: an integer indicating which segment of the target file this
+**        compressor_thread is responsible for encoding.  The id is 0-indexed,
+**        so the worker responsible for the 1st segment of the target file has
+**        a worker_id of 0, the worker responsible for the 2nd segment has id 1,
+**        and so on.
+**      work_orders_for_file: an array of long integers, which are offsets (in
+**        bytes) into the target file (from the start, at 0 bytes offset).
+**        These indicate which segments of the target file each
+**        compressor_thread is responsible for encoding.  In general, the thread
+**        with worker_id i is responsible for the bytes starting after the block
+**        of like characters at offset work_orders_for_file[i], up until the
+**        first byte after the block of like characters at offset
+**        work_orders_for_file[i+1].  The exceptions are the compressor_thread
+**        with worker_id 0, which is responsible for the first segment of the
+**        target file and thus is responsible for the bytes starting at offset
+**        work_orders_for_file[0] rather than the bytes after the indicated
+**        block, and the compressor_thread with worker_id num_jobs-1, which is
+**        responsible for the last segment of the target file and is thus
+**        responsible for the bytes beginning at the block after
+**        work_orders_for_file[num_jobs-1] up until the end of the file.
+**      num_jobs: an integer, indicating the total number of compressor_threads
+**        working on the given target file.
+**      target_file_name: a c-string, giving the name of the target file that
+**        this this compressor_thread is being directed to compress.
+**      completion_indicator: a pointer to a semaphore, which the
+**        compressor_thread uses to the main thread that it's done encoding its
+**        portion of the target file.
 **
+**    output (via void-casted pointer to a compthread_return struct):
+**      num_entries: an unsigned integer, indicating the total number of like-
+**        character blocks that were found in the bytes encoded by this
+**        compressor_thread.
+**      buffer: a character array, which stores the bytes that need to be sent
+**        to stdout as a result of the work done by this compressor_thread.
 **
+**  Each compressor_thread starts by opening its own pointer to the target file,
+**    and setting its offset to the position indicated by the work-order
+**    corresponding to its id.  Then, if it's not the thread responsible for
+**    encoding the first segment of the file, it scans forward in the file until
+**    it finds a change in characters (so that it can guarantee that it's not
+**    encoding part of a run of characters that the previous thread is
+**    responsible for).
+**  Once it finishes setting up, it performs run-length encoding, storing the
+**    results in a heap-allocated buffer in the format in which they will be
+**    sent to stdout (one 4-byte integer encoding followed by a 1-byte character
+**    encoding for each run of like characters).  The buffer starts at 5 bytes,
+**    and is doubled whenever it gets filled up, to try to balance the cost of
+**    allocating large amounts of unnecesary memory vs the cost of performing
+**    many memory allocations.
+**  After it reaches the end of the segment of the target file its responsible
+**    for, the compressor_thread closes its file pointer, creates a
+**    compthread_return struct on the heap to hold its return values, and
+**    returns a void-casted pointer to the struct (available to the main thread
+**    via joining).  The main thread is then responsible for freeing the space
+**    used by both the returned buffer and the compthread_return struct.
 */
 
-// main: create thread calling compress_file, passing the file name for each input
-// set up ^ so that work_orders[i] is the 'start' of the work for thread i
-// thread i compresses from byte i to byte work_orders[i+1], then finishes the block of identical characters that contains the char at work_orders[i+1]
-
-
 struct compthread_args {
-  int worker_id;            // starts at 0
-  long* work_orders_for_file;        // a pointer to the work_orders for target_file_name
-  int num_jobs;             // total # of threads, so each thread can tell if its the last one
-  char* target_file_name;   // the name of the file to compress
+  int worker_id;
+  long* work_orders_for_file;
+  int num_jobs;
+  char* target_file_name;
   sem_t* completion_indicator;
 };
 
@@ -50,57 +95,65 @@ struct compthread_return {
   unsigned num_entries;
   char* buffer;
 };
-//  expects arg to be a pointer to a valid compthread_arg struct (?)
-//  we need id, the list of job sizes (but that can be public), and the file name (can also be public)
-//  so all we really need, strictly speaking, is the id
-
-// but we should probably pass arguments by struct anyway
-
-//TODO: add to README that we're assuming this runs on an SSD (parallel reads)
 
 void* compressor_thread(void* arg) {
+    //cast arg for later use
     struct compthread_args* args = (struct compthread_args*) arg;
+
+    //open a stream to the target file
     FILE *fp;
     const char* mode = "r";
-    // printf("Worker %d is opening file: %s\n", args->worker_id, args->target_file_name);
-
     fp = fopen(args->target_file_name, mode);
     assert(fp != NULL);
 
+    //seek to the position in the file indicated by our work-order
     int rc = fseek(fp, args->work_orders_for_file[args->worker_id], SEEK_SET);
     assert(rc == 0);
 
+    // get the first character at the work order
     char curr_char = (char) fgetc(fp);
     char new_char;
 
-    if (args->worker_id != 0) { // do the scan-forward thing
-      // printf("Scanning forward to the end of the block\n");
+    // if we're not the first worker, scan forward in the file to get the
+    //  first character we're actually responsible for.
+    if (args->worker_id != 0) {
       for (new_char = fgetc(fp); new_char == curr_char; new_char = fgetc(fp));
       curr_char = new_char;
     }
 
+    // set up the buffer where we'll store our output (in the format in which
+    //  it needs to be sent to stdout)
     size_t buffer_size = 5;
     unsigned num_entries = 0;
     char* buffer = (char*) calloc(buffer_size, sizeof(char));
 
     uint32_t curr_char_count;
 
-    // printf("beginning compresson loop\n");
-    while ((curr_char != EOF) && ((args->worker_id == args->num_jobs-1) || (ftell(fp) <= args->work_orders_for_file[args->worker_id+1]))) {
-        // ((args->worker_id != args->num_jobs-1) && (ftell(fp) > args->work_orders_for_file[args->worker_id+1])) ||
-        // stop compression loop if:
-        // if we are the last worker
-            // we hit an EOF
-        // if we are NOT the last worker
-            // we have entered the block where the next guy would start
+    while (1) { // loop until:
+
+        // 1. We hit EOF
+        if (curr_char == EOF) {
+          break;
+        }
+
+        // 2. we hit the first character the next worker is responsible for
+        //  (only if we're not the last worker)
+        if ((args->worker_id != args->num_jobs-1) &&
+              (ftell(fp) >= args->work_orders_for_file[args->worker_id+1])) { // if any other thread reaches the block that the next thread will start at
+          break;
+        }
+
+        // count how many characters are in the current run
         for (curr_char_count = 1; (new_char = fgetc(fp)) == curr_char; curr_char_count++);
 
-        // if the buffer is too small, expand it
+        // if necessary, double the size of the output buffer
         if (buffer_size <= num_entries * 5) {
             buffer_size = buffer_size * 2;
             buffer = (char*) realloc(buffer, buffer_size);
         }
 
+        // store the information we gathered on the current run in the output
+        //  buffer
         buffer[num_entries*5]   = ((char*) &curr_char_count)[0];
         buffer[num_entries*5+1] = ((char*) &curr_char_count)[1];
         buffer[num_entries*5+2] = ((char*) &curr_char_count)[2];
@@ -111,28 +164,61 @@ void* compressor_thread(void* arg) {
     }
     fclose(fp);
 
+    // create the compthread_return struct we use to store our output on the
+    //  heap, and set its values to what we need to return.
     struct compthread_return* rvals = calloc(1, sizeof(struct compthread_return));
     rvals->num_entries = num_entries; rvals->buffer = buffer;
 
+    // tell main that we're done, and exit.
     sem_post(args->completion_indicator);
     return (void*) rvals;
 }
-/*
-// printing this should be
-struct compressor_return {
-  long long length;
-  uint32* counts;
-  char* chars;
-}*/
 
-//Compressor-thread returns (via join) a void-casted pointer to (? thing you're supposed to write)
-
-// we get an input AND output thread?!?! Then we should definitely use an output thread. . .
-// think about how to make that work . . . the issue is still that thread 1 has to complete
+/*  function: main
+**    inputs (via command line):
+**      A series of file names, indicating which files pzip should compress.
+**    output (via stdout):
+**      A series of bytes which is the run-length encoding of the input files.
+**      Each input file is encoded seperately, but the results are concatenated
+**      together.  Thus, if one input file ends with the same character that the
+**      next input file begins with, both characters will be encoded as though
+**      they were different characters.
+**      Each run of like characters is encoded in 5 bytes.  The first 4 bytes
+**      correspond to the bytes of a 32-bit unsigned integer giving the length
+**      of the run.  The last byte corresponds to the character repeated in the
+**      run.
+**
+**  main begins by getting the environment variable NTHREADS, which indicates
+**    the number of compressor_threads that can be run in parallel during the
+**    compression process, defaulting to 1 if NTHREADS is undefined.
+**  Next, main initializes an array of the compthread_args structs used for
+**    each compressor_thread that will used during the compression process.
+**    Each input file is assigned NTHREADS compressor_threads, each of which
+**    is responsible for an even portion of that file's bytes.
+**  Next, main sets up a pthread array, containing a pthread object for each
+**    compressor_thread that will be run (so that it can be joined, giving
+**    main access to its output).
+**  Once main is done initializing, it starts running compressor_threads.  As
+**    long as there are compressor_threads yet to be run, it runs the next
+**    compressor_thread scheduled to be run and then checks to see if the next
+**    thread whose output it needs to write has finished running.  If it has,
+**    main writes the output of that thread to stdout, and then frees the memory
+**    used to return the output.  If it hasn't finished, main loops back and
+**    waits until less than NTHREADS compressor_threads are running (using a
+**    semaphore initalized to NTRHEADS, which main waits on before starting a
+**    new compressor_thread and which each compressor_thread posts on
+**    immediately before returning).
+**  After all compressor_threads that will need to be started by main have been
+**    started, main waits until each thread whose output it still needs to write
+**    finishes in turn, writing the output to stdout and then freeing the memory
+**    used to return it.
+**  Once main finishes writing all output, it frees all the remaining memory
+**    that needs to be freed, and exits.
+*/
 
 int main(int argc, char** argv) {
-    // int NUM_CORES = get_nprocs_conf();
-    // printf("This system has %d processors configured and %d processors available.\n", NUM_CORES, get_nprocs());
+
+    // nthreads defaults to 1 if the NTHREADS environment variable is not defined
     char *nthreads_s = getenv("NTHREADS");
     long nthreads_l;
     if(nthreads_s == NULL) {
@@ -140,14 +226,25 @@ int main(int argc, char** argv) {
     } else {
         nthreads_l = atoi(nthreads_s);
     }
-    // printf("NTHREADS: %ld\n", nthreads_l);
 
+    // available_threads is a semaphore to control the number of simultaneously running threads
     sem_t available_threads;
     sem_init(&available_threads, 0, (unsigned int) nthreads_l);
 
+    // all_thread_args is an array of the arguments for compressor_thread that
+    //  will be run in the course of compressing the input files.  Each file
+    //  will be compressed by nthreads_l compressor_threads, in an attempt to
+    //  ensure full resource utilization.
     struct compthread_args* all_thread_args = (struct compthread_args*) calloc((argc-1)*nthreads_l, sizeof(struct compthread_args));
-    // printf("created args array\n");
+
+    // initalizing all_thread_args.  The compthread_args structs for each file
+    //  share a work_orders_for_file array (allocated on the heap) and a
+    //  target_file_name (stored in argv) - the rest of the data is duplicated.
+
+    // looping over each input file:
     for(int j = 1; j < argc; j++) {
+
+        // setting up the work_orders array for that file
         long size_of_file = file_size(argv[j]);
         long* work_orders = calloc(nthreads_l, sizeof(long));
         long work = 0;
@@ -155,6 +252,8 @@ int main(int argc, char** argv) {
           work_orders[i] = work;
           work = work + size_of_file / nthreads_l;
         }
+
+        //looping over the compthread_args structs for the current file:
         for (int i = 0; i < nthreads_l; i++) {
           all_thread_args[(j-1) * nthreads_l + i].worker_id = i;
           all_thread_args[(j-1) * nthreads_l + i].work_orders_for_file = work_orders;
@@ -163,10 +262,23 @@ int main(int argc, char** argv) {
           all_thread_args[(j-1) * nthreads_l + i].completion_indicator = &available_threads;
         }
     }
-    // printf("initialized args\n");
+
+    // threads is an array of pthread_t objects, stored so that they can be
+    //  joined by main when it's time to write their output to stdout.
+    // output_head indicates the next thread in threads whose output needs to
+    //  be written.
+    // next_thread indicates the next thread in threads that main should start,
+    //  when there are less than NTHREADS compressor_threads running.
     pthread_t* threads = (pthread_t*) calloc((argc-1) * nthreads_l, sizeof(pthread_t));
     int output_head = 0;
-    int next_thread = 0; // index into all_thread_args. points to the args for the next thread being created
+    int next_thread = 0;
+
+    //as long as more threads need to be run:
+    //  1. wait for the space to run a new thread to become available
+    //  2. start a new thread running
+    //  3. try to join the next thread whose ouput needs to be written.  If the
+    //    join is successful, write its output, then free the memory used to
+    //    return it.
     while (next_thread < (argc-1) * nthreads_l) {
         sem_wait(&available_threads);
         pthread_create(&threads[next_thread], NULL, compressor_thread, (void*) &all_thread_args[next_thread]);
@@ -179,24 +291,28 @@ int main(int argc, char** argv) {
             output_head++;
         }
     }
+
+    //After all threads have been started, while there are still threads whose
+    //  output hasn't been written:
+    //  1. join the next thread whose output needs to be written
+    //  2. write its output, then free the memory used to return it.
     while (output_head < (argc - 1) * nthreads_l) {
-      // printf("joining thread %d\n", output_head);
       void* void_retval;
       pthread_join(threads[output_head], &void_retval);
-      // printf("building retval\n");
-      struct compthread_return* retval = (struct compthread_return*) void_retval; //TODO: maybe this should be a function?
-      // printf("done\n");
-      // printf("number of entries reported: %u\n", retval->num_entries);
+      struct compthread_return* retval = (struct compthread_return*) void_retval;
       fwrite((void*) retval->buffer, 1, retval->num_entries*5, stdout);
-
       free(retval->buffer); free(retval);
       output_head++;
     }
+
+    // free the memory used to store the thread arguments, then destroy the semaphore
     free(threads);
     for(int j = 1; j < argc; j++) {
       free(all_thread_args[(j-1) * nthreads_l].work_orders_for_file);
     }
     free(all_thread_args);
     sem_destroy(&available_threads);
+
+    // exit in triumph, knowing that you have performed your duty with honor.
     return 0;
 }
